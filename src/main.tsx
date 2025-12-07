cd09d7fe6a97c0a00b7da5ade7fec2403e3ed15f
cstebner <chris.stebner@gmail.com>
2025-12-06 20:16:16 -0700

    Add Latex to GUI

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ChatProvider } from "./state/chatContext";
+import "katex/dist/katex.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
